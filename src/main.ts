ede1e272457fbba74749bccd2ba6e36eb19309e4
IMChiodo <imchiodo1@gmail.com>
2023-07-28 20:35:39 -0300

    port change

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   dotenv.config();
-  await app.listen(3003);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
